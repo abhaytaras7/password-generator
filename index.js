a4a016e51b9e2eb6fd71a01870935039aaa69e08
Om Sarraf <itsomsarraf.github@gmail.com>
2022-08-17 19:24:48 +0530

    new changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
 "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let passEl=document.getElementById("pass1")
+let passEl1=document.getElementById("pass1")
 
 function getRandomChar(){
     let index= Math.floor( Math.random()*characters.length )
@@ -16,4 +16,20 @@ function generateRandomPassword() {
     }
     return randomPassword
 }
-passEl.textContent = generateRandomPassword()
+
+let passEl2=document.getElementById("pass2")
+function getRandomChar(){
+    let index= Math.floor( Math.random()*characters.length )
+    return characters[index]
+}
+function generateRandomPassword() {
+    let randomPassword = ""
+    for (let i = 0; i<8; i++) {
+        randomPassword += getRandomChar()         
+    }
+    return randomPassword
+}
+function buttonClick(){
+    passEl1.textContent = generateRandomPassword()
+    passEl2.textContent = generateRandomPassword()
+}
